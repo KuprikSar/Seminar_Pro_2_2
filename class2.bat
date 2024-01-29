9cd156ee5e1dd18d6eea9742c5edc7ddee759361
VLADIMIR <sushonui@gmail.com>
2024-01-30 01:09:18 +0300

    first commit

---
+++ class2.bat
@@ -0,0 +1,2 @@
+gcc.exe -Wall -std=c99 -g -IC:\Users\KUPRIK\PDCurses-master -c class2.c -o class2.o
+gcc.exe -o class2.exe class2.o C:\Users\KUPRIK\PDCurses-master\wincon\pdcurses.a -IC:\Users\KUPRIK\PDCurses-master
